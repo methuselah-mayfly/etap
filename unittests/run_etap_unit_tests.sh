d310882878455b95d13468b72d5479a7fdecf5c8
Peter Kraak <pkraak@enphaseenergy.com>
2019-08-02 19:43:09 +1200

    sanitised paths in test data, fix path dependent bug in test all script.

--- unittests/run_etap_unit_tests.sh
+++ unittests/run_etap_unit_tests.sh
@@ -2,23 +2,22 @@
 # script to run all etap unit tests and check they output the correct results
 export SRC_DIR=$1
 
-echo "testing everything...\n"
+echo -e "testing all unit tests produce expected output, including deliberately failing ones...\n"
 
+strip-path () {
+  local FILE=$1
+
+  # elide build specific path so that the diff does not fail from path differences
+  sed 's|/.*\(/etap/unittests/\)|...\1|g' ${FILE}
+}
 
 RESULT=0
-for t in infra failing passing
+for t in failing passing infra
 do
   "./etap_${t}_test" > "${t}_output.txt"
-  diff "${t}_output.txt" "${SRC_DIR}/${t}_test_expected_data.txt" || RESULT=1
-  #RESULT=$?
-  # fold in the result, remembering _any_ failures
-  #echo "result [$RESULT] last [$?]"
-  #((RESULT=RESULT && $?))
-  echo "now result [$RESULT] last [$?]"
+  # we check the test produces the same output as a hand checked recording.
+  # fold also in the result, remembering _any_ failures so we can fail the script return if _any_ have failed.
+  diff <(strip-path "${t}_output.txt") <(strip-path "${SRC_DIR}/${t}_test_expected_data.txt") || RESULT=1
 done
 
-#((RESULT)) && echo "and result"
-#((RESULT)) || echo "or result"
-
-echo "result [${RESULT}]"
 exit $RESULT
